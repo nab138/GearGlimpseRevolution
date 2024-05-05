8896448905118878dad617a9c403d22b2bcaca95
nab138 <56705177+nab138@users.noreply.github.com>
2024-05-04 20:56:22 -0400

    FIELD RENDERS!

--- RootViewController.swift
+++ RootViewController.swift
@@ -1,9 +1,12 @@
 import UIKit
 import ARKit
+import SceneKit
+import SceneKit.ModelIO
 
 class RootViewController: UIViewController {
 
     var sceneView: ARSCNView!
+    var fieldNode: SCNNode!
 
     override func loadView() {
         super.loadView()
@@ -16,12 +19,38 @@ class RootViewController: UIViewController {
         super.viewDidLoad()
 
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
         sceneView.session.run(configuration)
 
-        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
-        let node = SCNNode()
-        node.geometry = box
-        node.position = SCNVector3(0, 0, -0.2)
+        NSLog("loading field model")
+        guard let field = loadModelFromName("Field3d_2024") else {
+            NSLog("Failed to load field model")
+            return
+        }
+        fieldNode = field
+        fieldNode.scale = SCNVector3(0.1, 0.1, 0.1)
+        NSLog("Field loaded successfully")
+
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        let tapLocation = sender.location(in: sceneView)
+        // Use raycastQuery to find the position of the tap in the real world
+        guard let query = sceneView.raycastQuery(from: tapLocation, allowing: .estimatedPlane, alignment: .horizontal) else {
+            return
+        }
+        
+        let results = sceneView.session.raycast(query)
+        guard let result = results.first else {
+            return
+        }
+
+        let position = SCNVector3(result.worldTransform.columns.3.x, result.worldTransform.columns.3.y, result.worldTransform.columns.3.z)
+
+        let node = fieldNode.clone()
+        node.position = position
 
         sceneView.scene.rootNode.addChildNode(node)
     }
@@ -29,5 +58,19 @@ class RootViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         sceneView.session.pause()
+    }
+
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+
+        sceneView.frame = CGRect(origin: .zero, size: size)
+    }
+
+    func loadModelFromName(_ name: String) -> SCNNode? {
+        let url = Bundle.main.url(forResource: name, withExtension: "usdz")!
+        let mdlAsset = MDLAsset(url: url)
+        mdlAsset.loadTextures()
+        let scene = SCNScene(mdlAsset: mdlAsset)
+        return scene.rootNode
     }
 }
