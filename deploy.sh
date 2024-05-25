4b9548171b6cf6a149d9e1f775e9ea8acbbf94aa
nab138 <56705177+nab138@users.noreply.github.com>
2024-05-25 11:54:06 -0400

    Fully automate development workflow

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+else
+  echo ".env file not found"
+  exit 1
+fi
+
 mv packages/*.ipa packages_archive/
 make package
 package_name=$(ls packages/*.ipa | awk -F'/' '{print $2}')
